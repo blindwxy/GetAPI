06575cfdea9132fd6c775ccfafb78f0c6453e28f
Xinying Wu <60805428+blindwxy@users.noreply.github.com>
2023-03-21 14:40:14 +0800

    Update getname.py

--- getname.py
+++ getname.py
@@ -8,7 +8,6 @@ for r in range(log_csv.shape[0]):  #log_csv.shape[0]为dataframe行数
     to_split = log_csv.iloc[[r], [4]].values[0][0]+'/0' #取出表格中第r行第c列的值得列表（因为dataframe中每个单元格是一个列表，数值在第一行第一列，所以[0][0]取之）
     temp_list = to_split.split('orgs/')   #to_split为字符串，用.split(":")把字符串以冒号为分界点分开为两个字符串，存储在一个列表temp_list中
     log_csv.iloc[[r],[4]] = temp_list[1]   #temp_list第2个值为所需的数值，取之赋给原表对应单元格，行列循环遍历后即可
-#print(log_csv)
+
 log_csv.to_csv('/Users/wuxinying/PycharmProjects/GithubAPI/jian3.csv')
 123
-456
